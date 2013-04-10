df3ac3216580cecf01abdcb8fd6811f69b04330a
olegator-sol@ya.ru <olegator-sol@ya.ru@22ff23ff-22a1-4bb5-a0e7-75dfe35ced9d>
2013-04-10 20:17:12 +0000

    1) Убрано INI из устройств 2) Пропускается устройство, которого нет в Modules 3) При конфигурировании канала в поля описание и модификатор подгружается значение выбраного канала 4) 2 кнопки  - конф выбранный канал - F9, конф выбранное устройтво - F8 5) Перенесена кнопка "F3 Устройства" в режим Управление "F3 Наладка" 6) Исправлен баг с генерацией пути.

---
+++ phx-cnc-ui-mainform/src/CXChannelEdit.h
@@ -0,0 +1,57 @@
+#ifndef CXCHANNELEDIT_H
+#define CXCHANNELEDIT_H
+
+#include <QDialog>
+#include "ui_CXChannelEdit.h"
+
+#include "SXChannelData.h"
+
+/*!
+	Класс для редактирования данных канала устройства.
+*/
+class CXChannelEdit : public QDialog, public Ui::CXChannelEdit
+{
+	Q_OBJECT
+
+public:
+	//! Конструктор.
+	CXChannelEdit(QWidget* parent = 0);
+
+	//! Деструктор.
+	~CXChannelEdit();
+
+	/*!
+		Функция установки объекта канала для редактирования.
+		\param aData - экземпляр объект канала.
+	*/
+	void setData(const SXChannelData& aData);
+
+	/*
+		Функция получения редактируемого объекта канала.
+		\return Экземпляр объект канала с новыми данными.
+	*/
+	const SXChannelData& getData();
+
+	/*!
+		Функция установки данных для секций и наименований.
+		\param aSectionsMap - карта связей между секцией и наименованиями.
+	*/
+	void setSectionData(const QMap <QString, QString>& aSectionsMap);
+
+public slots:
+	//! Переопределенный слот применения изменений.
+	virtual void accept();
+
+private slots:
+	//! Слот на изменение текущей секции.
+	void onSectionChange();
+
+	//! Слот на изменение текущего имени.
+	void onNameChange();
+
+private:
+	SXChannelData mChannelData;
+	QMap <QString, QString> mSectionsMap;
+};
+
+#endif // CXCHANNELEDIT_H
---
+++ phx-cnc-ui-mainform/src/CXDeviceEdit.cpp
@@ -0,0 +1,102 @@
+#include "CXDeviceEdit.h"
+
+#include <QMessageBox>
+
+#include "CXTouchButton.h"
+
+CXDeviceEdit::CXDeviceEdit(QWidget* parent) : QDialog(parent)
+{
+	setWindowTitle(trUtf8("Редактор устройства"));
+	setWindowFlags(Qt::Dialog | Qt::WindowCloseButtonHint);
+
+	QVBoxLayout* layout = new QVBoxLayout(this);
+	layout->setMargin(7);
+	layout->setSpacing(7);
+
+	mCentralLayout = new QGridLayout;
+	mCentralLayout->setMargin(0);
+	mCentralLayout->setSpacing(7);
+
+	layout->addLayout(mCentralLayout);
+
+	CXTouchButton* acceptButton = new CXTouchButton(trUtf8("Применить"), this);
+	layout->addWidget(acceptButton);
+
+	mValidator = new QRegExpValidator(QRegExp("[^=,]*"), this);
+
+	connect(acceptButton, SIGNAL(clicked()), this, SLOT(accept()));
+}
+
+CXDeviceEdit::~CXDeviceEdit()
+{
+
+}
+
+void CXDeviceEdit::setData(const QString& aData)
+{
+	QStringList dataList = aData.split(",");
+
+	while (mEditList.count() > dataList.count())
+	{
+		const SXDeviceEditors& deviceEditors = mEditList.at(mEditList.count() - 1);
+
+		delete deviceEditors.mLineEdit;
+		delete deviceEditors.mLabel;
+
+		mEditList.remove(mEditList.count() - 1);
+	}
+
+	while (mEditList.count() < dataList.count())
+	{
+		SXDeviceEditors deviceEditors;
+
+		deviceEditors.mLabel = new QLabel(this);
+		deviceEditors.mLineEdit = new QLineEdit(this);
+		deviceEditors.mLineEdit->setValidator(mValidator);
+
+		mCentralLayout->addWidget(deviceEditors.mLabel, mEditList.count(), 0);
+		mCentralLayout->addWidget(deviceEditors.mLineEdit, mEditList.count(), 1);
+
+		mEditList.append(deviceEditors);
+	}
+
+	for (int i = 0; i < dataList.count(); i++)
+	{
+		QString curData = dataList.at(i);
+		const SXDeviceEditors& deviceEditors = mEditList.at(i);
+		deviceEditors.mLabel->setText(curData.left(curData.indexOf("=")));
+		deviceEditors.mLineEdit->setText(curData.mid(curData.indexOf("=") + 1));
+	}
+}
+
+QString CXDeviceEdit::getData()
+{
+	return mDeviceData;
+}
+
+void CXDeviceEdit::accept()
+{
+	mDeviceData = "";
+
+	for (int i = 0; i < mEditList.count(); i++)
+	{
+		const SXDeviceEditors& curData = mEditList.at(i);
+
+		if (curData.mLineEdit->text().isEmpty())
+		{
+			QMessageBox::critical(this, trUtf8("Ошибка"), trUtf8("Введите данные поля %1.").arg(curData.mLabel->text()));
+
+			curData.mLineEdit->setFocus();
+
+			return;
+		}
+
+		if (!mDeviceData.isEmpty()) mDeviceData.append(",");
+
+		mDeviceData.append(curData.mLabel->text());
+		mDeviceData.append("=");
+		mDeviceData.append(curData.mLineEdit->text());
+	}
+
+	QDialog::accept();
+}
