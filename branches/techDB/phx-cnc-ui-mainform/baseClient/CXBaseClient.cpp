f833836e3b309565cb7b73f64548992f8b0438fb
alexander.kulpanov <alexander.kulpanov@22ff23ff-22a1-4bb5-a0e7-75dfe35ced9d>
2014-03-21 13:02:37 +0000

    alpha.10

--- branches/techDB/phx-cnc-ui-mainform/baseClient/CXBaseClient.cpp
+++ branches/techDB/phx-cnc-ui-mainform/baseClient/CXBaseClient.cpp
@@ -16,112 +16,130 @@ CXBaseClient::~CXBaseClient()
 
 }
 
-QString CXBaseClient::connectToDataBase(const QString& aIP, const QString& aDBName, int aPort, const QString& aUser, const QString& aPassword)
+QString
+CXBaseClient::connectToDataBase(const QString& aIP, const QString& aDBName,
+    int aPort, const QString& aUser, const QString& aPassword)
 {
-	if (mDataBase.isOpen()) mDataBase.close();
-/*
-	mDataBase = QSqlDatabase::addDatabase("QPSQL");
-	mDataBase.setHostName(aIP);
-	mDataBase.setDatabaseName(aDBName);
-	mDataBase.setUserName(aUser);
-	mDataBase.setPassword(aPassword);
-*/
+  if (mDataBase.isOpen())
+    mDataBase.close();
+  /*
+   mDataBase = QSqlDatabase::addDatabase("QPSQL");
+   mDataBase.setHostName(aIP);
+   mDataBase.setDatabaseName(aDBName);
+   mDataBase.setUserName(aUser);
+   mDataBase.setPassword(aPassword);
+   */
 
-	mDataBase = QSqlDatabase::addDatabase("QSQLITE");
-	mDataBase.setDatabaseName(aDBName);
+  mDataBase = QSqlDatabase::addDatabase("QSQLITE");
+  mDataBase.setDatabaseName(aDBName);
 
-	bool ok = mDataBase.open();
+  bool ok = mDataBase.open();
 
-	if (!ok)
-	{
-		return mDataBase.lastError().text();
-	}
+  if (!ok)
+    {
+      return mDataBase.lastError().text();
+    }
 
-	return QString();
+  return QString();
 }
 
-KeyValueList CXBaseClient::execute(const QString& aTableName, const QString& aColumns, const QString& aWhere, const QString& aOrder)
+KeyValueList
+CXBaseClient::execute(const QString& aTableName, const QString& aColumns,
+    const QString& aWhere, const QString& aOrder)
 {
-	KeyValueList values;
+  KeyValueList values;
 
-	QString queryString = QString("SELECT %1 FROM %2").arg(aColumns).arg(aTableName);
+  QString queryString = QString("SELECT %1 FROM %2").arg(aColumns).arg(
+      aTableName);
 
-	if (!aWhere.isEmpty()) queryString.append(QString(" WHERE %1").arg(aWhere));
-	if (!aOrder.isEmpty()) queryString.append(QString(" ORDER BY %1").arg(aOrder));
+  if (!aWhere.isEmpty())
+    queryString.append(QString(" WHERE %1").arg(aWhere));
+  if (!aOrder.isEmpty())
+    queryString.append(QString(" ORDER BY %1").arg(aOrder));
 
-	queryString.append(";");
+  queryString.append(";");
 
-	QSqlQuery query(mDataBase);
-	if (query.exec(queryString))
-	{
-		while (query.next())
-		{
-			//values.insert(query.value(1).toString(), query.value(0).toString());
-			values.append(Pair(query.value(1).toString(), query.value(0).toString()));
-		}
-	}
+  QSqlQuery query(mDataBase);
+  if (query.exec(queryString))
+    {
+      while (query.next())
+        {
+          //values.insert(query.value(1).toString(), query.value(0).toString());
+          values.append(
+              Pair(query.value(1).toString(), query.value(0).toString()));
+        }
+    }
 //	else query.lastError().text();
 
-	return values;
+  return values;
 }
 
-KeyValueMap CXBaseClient::getValues(const QString& aTableName, const QString& aColumns, const QString& aWhere)
+KeyValueMap
+CXBaseClient::getValues(const QString& aTableName, const QString& aColumns,
+    const QString& aWhere)
 {
-	KeyValueMap values;
+  KeyValueMap values;
 
-	QString queryString = QString("SELECT %1 FROM %2").arg(aColumns).arg(aTableName);
+  QString queryString = QString("SELECT %1 FROM %2").arg(aColumns).arg(
+      aTableName);
 
-	if (!aWhere.isEmpty()) queryString.append(QString(" WHERE %1").arg(aWhere));
+  if (!aWhere.isEmpty())
+    queryString.append(QString(" WHERE %1").arg(aWhere));
 
-	queryString.append(";");
+  queryString.append(";");
 
-	QSqlQuery query(mDataBase);
-	if (query.exec(queryString) && query.first())
-	{
-		QSqlRecord record = query.record();
-		for (int i = 0; i < record.count(); i++)
-		{
-			values.insert(record.fieldName(i), query.value(i).toString());
-		}
-	}
+  QSqlQuery query(mDataBase);
+  if (query.exec(queryString) && query.first())
+    {
+      QSqlRecord record = query.record();
+      for (int i = 0; i < record.count(); i++)
+        {
+          values.insert(record.fieldName(i), query.value(i).toString());
+        }
+    }
 //	else query.lastError().text();
 
-	return values;
+  return values;
 }
 
-KeyValueMap CXBaseClient::getImageData(const QString& aTableName, const QString& aColumns, const QString& aWhere, const QString& aFileName)
+KeyValueMap
+CXBaseClient::getImageData(const QString& aTableName, const QString& aColumns,
+    const QString& aWhere, const QString& aFileName)
 {
-	KeyValueMap values;
+  KeyValueMap values;
 
-	QString queryString = QString("SELECT %1 FROM %2").arg(aColumns).arg(aTableName);
+  QString queryString = QString("SELECT %1 FROM %2").arg(aColumns).arg(
+      aTableName);
 
-	if (!aWhere.isEmpty()) queryString.append(QString(" WHERE %1").arg(aWhere));
+  if (!aWhere.isEmpty())
+    queryString.append(QString(" WHERE %1").arg(aWhere));
 
-	queryString.append(";");
+  queryString.append(";");
 
-	QSqlQuery query(mDataBase);
-	if (query.exec(queryString) && query.first())
-	{
-		QSqlRecord record = query.record();
-		for (int i = 0; i < record.count(); i++)
-		{
-			//бинарные данные картинки должны быть последними.
-			if (i == record.count() - 1)
-			{
-				QFile imageFile(aFileName);
-				if (imageFile.open(QIODevice::WriteOnly))
-				{
-					//QVariant v = query.value(i);
-					//QString s = query.value(i).toString();
-					QByteArray data = query.value(i).toByteArray();
-					imageFile.write(data);
-					imageFile.close();
-				}
-			}
-			else values.insert(record.fieldName(i), query.value(i).toString());
-		}
-	}
-	//	else query.lastError().text();
+  QSqlQuery query(mDataBase);
+  if (query.exec(queryString) && query.first())
+    {
+      QSqlRecord record = query.record();
+      for (int i = 0; i < record.count(); i++)
+        {
+          //бинарные данные картинки должны быть последними.
+          if (i == record.count() - 1)
+            {
+              QFile imageFile(aFileName);
+              if (imageFile.open(QIODevice::WriteOnly))
+                {
+                  //QVariant v = query.value(i);
+                  //QString s = query.value(i).toString();
+                  QByteArray data = query.value(i).toByteArray();
+                  imageFile.write(data);
+                  imageFile.close();
+                }
+            }
+          else
+            values.insert(record.fieldName(i), query.value(i).toString());
+        }
+    }
+  //	else query.lastError().text();
 
-	return values;
+  return values;
 }
