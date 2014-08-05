4b4fbf64e2a51b2a85c427b7c6c8bc94a9025563
alexander.kulpanov <alexander.kulpanov@22ff23ff-22a1-4bb5-a0e7-75dfe35ced9d>
2014-08-05 05:42:08 +0000

    separate funcs over files

---
+++ baseClient/getFromDB.cpp
@@ -0,0 +1,299 @@
+#include <QLibrary>
+
+#include <QDir>
+#include <QFile>
+#include <QFileInfo>
+#include <QTextStream>
+#include <QTextCodec>
+#include <QSqlQuery>
+#include <qdebug.h>
+#include <math.h>
+
+#include "CXBaseClient.h"
+#include "utils/CXParamData.h"
+#include "utils/iniFile.h"
+bool
+contains(const QString& aKey, KeyValueMap& aKeys,
+    const KeyValueList& aAvailableKeys, const QString& aType)
+{
+  if (!aKeys.contains(aKey))
+    return false;
+
+  QString keyValue = aKeys.value(aKey);
+
+  QString value;
+  foreach (Pair curValue, aAvailableKeys){
+    if (curValue.first == keyValue)
+    {
+      value = curValue.second;
+      break;
+    }
+  }
+
+  if (value.isEmpty())
+    return false;
+
+  QString res = aKeys.value(aKey);
+  aKeys.insert(aKey, value);
+
+  CXParamData::setValues(aType + "/Keys",
+      PairsList() << QPair<QString, QString>(aKey, res));
+
+  return true;
+}
+
+int
+check(const KeyValueList& aAvailableKeys, const QString& aCurKey,
+    KeyValueMap& aKeys, const QString& aType)
+{
+  if (aAvailableKeys.isEmpty())
+  {
+    CXParamData::close();
+    return -1;
+  }
+
+  QStringList list;
+  foreach (Pair curValue, aAvailableKeys){
+    list.append(curValue.first);
+  }
+
+  CXParamData::setKeysArray(aCurKey, list);
+
+  if (!contains(aCurKey, aKeys, aAvailableKeys, aType))
+  {
+    CXParamData::close();
+    return 0;
+  }
+
+  return 1;
+}
+
+
+int getFromDB(QTextStream& out, const QString& fileName, const QString& type){
+  /**/
+  CXParamData::open(fileName);
+  CXBaseClient client;
+  KeyValueMap dataBase = CXParamData::getValues("Database");
+  QString dbFileName =  dataBase.value("name");
+  QString resString = client.connectToDataBase(dataBase.value("hosts"), dataBase.value("name"),
+      dataBase.value("port").toInt(), dataBase.value("user"), dataBase.value("password"));
+  int res = 0;
+  do{
+    if (!resString.isEmpty())
+    {
+      out << "connection error:\n";
+      out << resString;
+      out << "\n";
+      res = -1;
+      break;
+    }
+
+    KeyValueMap keys = CXParamData::getValues(type + "/Keys");
+    CXParamData::deleteKeys(QStringList() << type);
+    /**/
+    QStringList allKeys;
+    allKeys <<  "Source/Keys" << "MetallType/Keys" << "Thickness/Keys" << "Power/Keys" << "GasTypes/Keys" << "ConsAngles/Keys" << type + "/Common";
+    CXParamData::deleteKeys(allKeys);
+
+    /**/
+    QString curKey = "Source";
+    KeyValueList availableKeys = client.execute("tbl_plasma_sources", "id, name", "", "name");
+
+    int res = check(availableKeys, curKey, keys, type);
+    if (res <= 0)
+    {
+      if (res < 0) out << QString("%1: available keys are empty\n").arg(curKey);
+      else out << QString("wrong key: %1-%2\n").arg(curKey).arg(keys.value(curKey));
+
+      res = -2;
+      break;
+    }
+
+    curKey = "MetallType";
+    QString w = QString("plasma_source=%1").arg(keys.value("Source"));
+    availableKeys = client.execute("tbl_plasma_metal_types as a, tbl_plasma_params as b", "DISTINCT metal_type, a.name", w + " AND metal_type=a.id", "a.name");
+
+    res = check(availableKeys, curKey, keys, type);
+    if (res <= 0)
+    {
+      if (res < 0) out << QString("%1: available keys are empty\n").arg(curKey);
+      else out << QString("wrong key: %1-%2\n").arg(curKey).arg(keys.value(curKey));
+      res = -3;
+      break;
+    }
+
+    curKey = "Thickness";
+    w += QString(" AND metal_type=%2").arg(keys.value("MetallType"));
+    availableKeys = client.execute("tbl_plasma_params", "DISTINCT min_thickness, min_thickness", w, "min_thickness");
+
+    res = check(availableKeys, curKey, keys, type);
+    if (res <= 0)
+    {
+      if (res < 0) out << QString("%1: available keys are empty\n").arg(curKey);
+      else out << QString("wrong key: %1-%2\n").arg(curKey).arg(keys.value(curKey));
+
+      res = -4;
+      break;
+    }
+
+    curKey = "Power";
+    w += QString(" AND max_thickness=%1").arg(keys.value("Thickness"));
+    availableKeys = client.execute("tbl_plasma_params", "DISTINCT amperage, amperage", w, "amperage");
+    res = check(availableKeys, curKey, keys, type);
+    if (res <= 0)
+    {
+      if (res < 0) out << QString("%1: available keys are empty\n").arg(curKey);
+      else out << QString("wrong key: %1-%2\n").arg(curKey).arg(keys.value(curKey));
+
+      res = -5;
+      break;
+    }
+
+    curKey = "GasTypes";
+    w += QString(" AND amperage=%1").arg(keys.value("Power"));
+    availableKeys = client.execute("tbl_gas_types as a, tbl_plasma_params as b", "DISTINCT b.gases, a.gases", w + " AND b.gases=a.id", "a.gases");
+
+    res = check(availableKeys, curKey, keys, type);
+    if (res <= 0)
+    {
+      if (res < 0) out << QString("%1: available keys are empty\n").arg(curKey);
+      else out << QString("wrong key: %1-%2\n").arg(curKey).arg(keys.value(curKey));
+
+      res = -6;
+      break;
+    }
+
+    curKey = "ConsAngles";
+    w += QString(" AND gases=%1").arg(keys.value("GasTypes"));
+    availableKeys = client.execute("tbl_cons_angles as a, tbl_plasma_params as b", "DISTINCT b.cons_angle, a.name", w + " AND b.cons_angle=a.id", "a.name");
+
+    res = check(availableKeys, curKey, keys, type);
+    if (res <= 0)
+    {
+      if (res < 0) out << QString("%1: available keys are empty\n").arg(curKey);
+      else out << QString("wrong key: %1-%2\n").arg(curKey).arg(keys.value(curKey));
+
+      res = -7;
+      break;
+    }
+
+    w += QString(" AND cons_angle=%1").arg(keys.value("ConsAngles"));
+
+    /* Загрузка значений полей */
+    KeyValueMap columnValuesMap;
+
+    columnValuesMap.insert("min_thickness", "MinThickness");
+    columnValuesMap.insert("max_thickness", "MaxThickness");
+
+    columnValuesMap.insert("amperage", "Power");
+    columnValuesMap.insert("out_description", "Description");
+    columnValuesMap.insert("out_under_water", "UnderWater");
+    columnValuesMap.insert("out_voltage", "SVRVoltage");
+    columnValuesMap.insert("out_feed", "CuttingFeed");
+    columnValuesMap.insert("out_burning_z_distance", "BurningZDistance");
+    columnValuesMap.insert("out_cutting_z_distance", "CuttingZDistance");
+    columnValuesMap.insert("out_kerf", "Kerf");
+    columnValuesMap.insert("out_ignition_distance", "IgnitionDistance");
+    columnValuesMap.insert("out_burning_feed", "BurningFeed");
+    columnValuesMap.insert("out_burning_time", "BurningTime");
+    columnValuesMap.insert("out_parts_group", "PartsNumber");
+    columnValuesMap.insert("out_gases_mix1", "Mixes1");
+    columnValuesMap.insert("out_gases_mix2", "Mixes2");
+    columnValuesMap.insert("out_plasma_preflow_pressure", "Gas1PreflowPressure");
+    columnValuesMap.insert("out_shield_preflow_pressure", "Gas2PreflowPressure");
+    columnValuesMap.insert("out_plasma_cutflow_pressure", "Gas1CutflowPressure");
+    columnValuesMap.insert("out_shield_cutflow_pressure", "Gas2CutflowPressure");
+    columnValuesMap.insert("out_plasma_preflow_rate", "Gas1PreflowRate");
+    columnValuesMap.insert("out_shield_preflow_rate", "Gas2PreflowRate");
+    columnValuesMap.insert("out_plasma_cutflow_rate", "Gas1CutflowRate");
+    columnValuesMap.insert("out_shield_cutflow_rate", "Gas2CutflowRate");
+
+    KeyValueMap values = client.getValues("tbl_plasma_params", QStringList(columnValuesMap.keys()).join(","), w);
+    if (!values.isEmpty())
+    {
+      PairsList resValues;
+      QMap<QString, QString>::iterator iter;
+      QString value;
+
+      for (iter = columnValuesMap.begin(); iter != columnValuesMap.end(); iter++)
+      {
+        value = values.value(iter.key());
+
+        if (value == "false") value = "0";
+        if (value == "true") value = "1";
+
+        resValues.append(QPair<QString,QString>(iter.value(), value));
+      }
+
+      CXParamData::setValues(type + "/Common", resValues);
+    }
+
+    /**/
+    /*
+   for (int i = 2; i <= 76; i++)
+   {
+   //client.getImageData("tbl_torch_parts_types as a, tbl_torch_parts as b", "a.name, a.description, b.image", QString("b.id=%1").arg(i), QString("d:/%1.bmp").arg(i));
+
+   QFile file(QString("d:/!images!/%1.bmp").arg(i));
+   if (!file.open(QIODevice::ReadOnly)) continue;
+   QByteArray byteArray = file.readAll();
+
+   QString queryString = QString("UPDATE tbl_torch_parts SET image=? WHERE id=?;");
+
+   QSqlQuery query;
+   query.prepare(queryString);
+   query.addBindValue(byteArray);
+   query.addBindValue(i);
+   bool b = query.exec();
+   b = b;
+   }
+     */
+    /* Загрузка картинок */
+    bool isBreak = false;
+    int imageIndex = 1;
+    KeyValueMap imageParts = client.getValues("tbl_parts_groups"
+        , "part_1,part_2,part_3,part_4,part_5,part_6,part_7,part_8,part_9,part_10"
+        , QString("id=%1").arg(values.value("out_parts_group")));
+    QString curPart;
+    QFileInfo fileInfo(dbFileName);
+
+    QDir imagesDir(QString("db/%1").arg(type));
+    if (!imagesDir.exists())
+      imagesDir.mkpath(QString("db/%1").arg(type));
+
+    while (!isBreak)
+    {
+      curPart = QString("part_%1").arg(imageIndex);
+      values = CXParamData::getValues(QString("%1/Image%2").arg(type).arg(imageIndex));
+
+      if (values.isEmpty() || !imageParts.contains(curPart)) isBreak = true;
+      else
+      {
+        values = client.getImageData("tbl_torch_parts_types as a, tbl_torch_parts as b"
+            , "a.name, a.description, b.number, b.image"
+            , "b.type = a.id AND b.id=" + imageParts.value(curPart)
+            , QString("db/%1/Image%2").arg(type).arg(imageIndex));
+
+        //          qDebug() << values;
+        PairsList imageValues;
+        imageValues << QPair<QString, QString>("image", QString("%1/Image%2").arg(type).arg(imageIndex));
+        QMap<QString, QString>::iterator iter;
+        for (iter = values.begin(); iter != values.end(); iter++)
+        {
+          if (iter.key() == QString("image")) continue;
+
+          imageValues << QPair<QString, QString>(iter.key(), iter.value());
+        }
+        //          qDebug() << imageValues;
+        CXParamData::setValues(QString("%1/Image%2").arg(type).arg(imageIndex), imageValues);
+      }
+
+      imageIndex++;
+    }
+  }while(0);
+
+  /**/
+  CXParamData::close();
+  return res;
+
+}
---
+++ baseClient/transferParams.cpp
@@ -0,0 +1,220 @@
+/*
+ * parseG59.cpp
+ *
+ *  Created on: Jul 31, 2014
+ *      Author: kulpanov
+ */
+
+
+#include <QDir>
+#include <QFile>
+#include <QFileInfo>
+#include <QTextStream>
+#include <QTextCodec>
+#include <qdebug.h>
+#include <math.h>
+
+#include "CXBaseClient.h"
+#include "utils/CXParamData.h"
+#include "utils/iniFile.h"
+
+
+//при необходимости раскидываем параметры плазмы
+int
+ReloadPlasmaCuttingParams(CIniFile& cutIni, CIniFile& moveIni, CIniFile& techIni, QString _name)
+{
+  string name = _name.toStdString() + "/Common";
+
+  int listFeed = cutIni.GetValueI(name, "CuttingFeed", 1000);
+  moveIni.SetValueI("Move/ListFeed", "value", listFeed);
+  moveIni.SetValueI("General/Offset", "value", qRound(cutIni.GetValueF(name, "Kerf", 0) * 10 / 2));
+
+  int burn_feed = cutIni.GetValueI(name, "BurningFeed", 0);
+
+  double zTouchUp = 0;
+  double zUpAfterArc = 0;
+  double zCutDistance = 0;
+  double motionDelay = qRound(cutIni.GetValueF(name, "BurningTime", 0) * 10);
+  string paramName = "Technology/" + _name.toStdString() + "/MotionDelay";
+  techIni.SetValueI(paramName, "value", (motionDelay >= 0) ? motionDelay : 0);
+  paramName = "Technology/" + _name.toStdString() + "/Burn/FeedDivisor";
+  techIni.SetValueI(paramName, "value", (int) burn_feed * 100 / listFeed);
+
+  zTouchUp = cutIni.GetValueF(name, "IgnitionDistance", 0) * 10;
+  paramName = "Technology/" + _name.toStdString() + "/TouchUp";
+  techIni.SetValueI(paramName, "value", zTouchUp);
+
+  zUpAfterArc = cutIni.GetValueF(name, "BurningZDistance", 0) * 10;
+
+  paramName = "Technology/" + _name.toStdString() + "/Burn/ZUp";
+  if (zUpAfterArc - zTouchUp > 0)
+    techIni.SetValueI(paramName, "value", (zUpAfterArc - zTouchUp));
+  else
+    techIni.SetValueI(paramName, "value", 0);
+
+  zCutDistance = cutIni.GetValueF(name, "CuttingZDistance", 0) * 10;
+
+  //высота реза
+  paramName = "Technology/" + _name.toStdString() + "/BurnZDistance";
+  techIni.SetValueI(paramName, "value", zCutDistance);
+
+  double zHunt = cutIni.GetValueF(name, "SVRVoltage", 0);
+
+  paramName = "Technology/" + _name.toStdString() + "/ZHunt";
+
+  techIni.SetValueF(paramName, "value", zHunt);
+
+  return 0;
+}
+
+////раскидываем параметры разметчика
+//int
+//ReloadWriterCuttingParams(CIniFile& _cutIni)
+//{
+//  int _listFeed = _cutIni.GetValueI("Writer/Common", "CuttingFeed", 1000);
+//  iniFileParams.SetValueI("Move/ListFeed", "value", _listFeed);
+//
+//  double zTouchUp = _cutIni.GetValueF("Writer/Common", "CuttingZDistance", 0)
+//              * 10;
+//  iniParamsTech.SetValueI("Technology/Writer/TouchUp", "value", zTouchUp);
+//
+//  double zHunt = _cutIni.GetValueF("Writer/Common", "SVRVoltage", 0);
+//
+//  iniParamsTech.SetValueF("Technology/Writer/ZHunt","value", zHunt);
+//
+//  //  if (SetZHuntValue(3, _cutIni.GetValueF("Writer/Common", "ArcVoltage", 0))
+//  //      != 0)
+//  //    return 1;
+//
+//  if (!iniParamsTech.WriteIniFile())
+//  {
+//    printf("ERROR: fault to write Ini file\n");
+//    iniParamsTech.ReadIniFile();
+//    return 1;
+//  }; //else SendReloadIniFile(iniParamsWriter.Path());
+//  return 0;
+//
+//}
+//
+////при необходимости раскидываем параметры кислорода
+//int
+//ReloadOxyCuttingParams(CIniFile& _cutIni)
+//{
+//  ///скидываем параметры с временного файла в файл инициализации
+//  int _listFeed = _cutIni.GetValueI("Oxy/Common", "CuttingFeed", 1000);
+//  iniFileParams.SetValueI("Move/ListFeed", "value", _listFeed);
+//  iniFileParams.SetValueI("General/Offset", "value",
+//      qRound(_cutIni.GetValueF("Oxy/Common", "Kerf", 0) * 10 / 2));
+//
+//  //файл для передачи параметров кислородной газовой консоли
+//  CIniFile iniParamsGC("./jini/paramsGC.ini", 1000);
+//  iniParamsGC.ReadFile("./jini/paramsGC.ini");
+//  ///настройка газовой консоли
+//  QString n_sup = "";
+//  for (int i = 0; i < MAX_SUPP_COUNT; i++)
+//  {
+//    if ((1 << i) & supp_mask)
+//    {
+//      n_sup = "Oxy/Support" + QString::number(i + 1);
+//      string n_sup_std = n_sup.toStdString();
+//      iniParamsGC.SetValueF(n_sup_std, "KP",
+//          _cutIni.GetValueF(n_sup_std, "KP", 0));
+//      iniParamsGC.SetValueF(n_sup_std, "KPV",
+//          _cutIni.GetValueF(n_sup_std, "KPV", 0));
+//      iniParamsGC.SetValueF(n_sup_std, "GG",
+//          _cutIni.GetValueF(n_sup_std, "GG", 0));
+//      iniParamsGC.SetValueF(n_sup_std, "GGV",
+//          _cutIni.GetValueF(n_sup_std, "GGV", 0));
+//    };
+//  };
+//
+//  iniParamsGC.SetValueF("Oxy/MOxy_Pressure", "value",
+//      _cutIni.GetValueF("Oxy/MOxy_Pressure", "value", 10));
+//  iniParamsGC.SetValueF("Oxy/MOxy_Pressure_Hi", "value",
+//      _cutIni.GetValueF("Oxy/MOxy_Pressure_Hi", "value", 10));
+//
+//  //iniParamsGC.SetValueF("Oxy/AccTimeMOxy", "value", iniParamsOxy.GetValueF("Technology/Oxy/WaitKr2","value", 10) );
+//
+//  if (!iniParamsGC.WriteIniFile())
+//  {
+//    printf("ERROR: fault to write Ini file\n");
+//    iniParamsGC.ReadIniFile();
+//    return 1;
+//  }; //else SendReloadIniFile(iniParamsGC.Path());
+//
+//  ///настройкм технологических параметров
+//  iniParamsTech.SetValueI("Technology/Oxy/WarmTime", "value",
+//      _cutIni.GetValueF("Oxy/Common", "WarmingTime", 60) * 10);
+//
+//  iniParamsTech.SetValueI("Technology/Oxy/Burn/Time", "value",
+//      _cutIni.GetValueF("Oxy/Common", "BurningTime", 2) * 10);
+//
+//  int burn_feed = _cutIni.GetValueI("Oxy/Common", "BurningFeed", 0);
+//  iniParamsTech.SetValueI("Technology/Oxy/Burn/FeedDivisor", "value",
+//      (int) burn_feed * 100 / _listFeed);
+//
+//
+//  double zHunt = _cutIni.GetValueF("Oxy/Common", "SVRVoltage", 0);
+//
+//  iniParamsTech.SetValueF("Technology/Oxy/ZHunt","value", zHunt);
+//
+//
+//  if (!iniParamsTech.WriteIniFile())
+//  {
+//    printf("ERROR: fault to write Ini file\n");
+//    iniParamsTech.ReadIniFile();
+//    return 1;
+//  }; //else SendReloadIniFile(iniParamsOxy.Path());
+//
+//  //  if(SetZHuntValue(supp_count, _cutIni.GetValueF(name, "SVRVoltage", 0)) != 0)
+//  //      return 1;
+//  return 0;
+//}
+
+//перегружаем параметры реза
+int transferToParams(QTextStream& out, const QString& fileName, const QString& type){
+  CIniFile cutIni(fileName.toStdString(), -1);
+  if (!cutIni.ReadFile(fileName.toStdString()))
+  {
+    out << "dbClient: "<< fileName << ": Unable read file";
+    return 1;
+  }
+  string path = "./jini/params.ini";
+  CIniFile iniFileParams(path);
+  if (!iniFileParams.ReadFile(path))
+  {
+    out << "dbClient: "<< iniFileParams.Path().c_str() << ": Unable to read file";
+    return 1;
+  }
+  path = "./jini/params" + type.toStdString() + ".ini";
+  CIniFile iniParamsTech(path);
+  if (!iniParamsTech.ReadFile(path))
+  {
+    out << "dbClient: "<< iniParamsTech.Path().c_str() << ": Unable to read file";
+    return 1;
+  }
+
+  //обновляем нужную технологию
+  int res = 0;
+  if(type == "MPlasma" || type == "Plasma")
+    res = ReloadPlasmaCuttingParams(cutIni, iniFileParams, iniParamsTech, type);
+//  else if (_type == "Oxy")
+//    res = ReloadOxyCuttingParams(cutIni);
+//  else
+//    res = ReloadWriterCuttingParams(cutIni);
+  if (res != 0)
+    return res;
+
+  if (!iniParamsTech.WriteIniFile())
+  {
+    out << "dbClient: "<< iniParamsTech.Path().c_str() << ": Unable to write file";
+    return 1;
+  }
+
+  if (!iniFileParams.WriteIniFile())
+  {
+    out << "dbClient: "<< iniFileParams.Path().c_str() << ": Unable to write file";
+    return 1;
+  }
+  return 0;
+}
