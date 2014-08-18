8bb31e968f11495fc85d85ccf36c70f815890230
alexander.kulpanov <alexander.kulpanov@22ff23ff-22a1-4bb5-a0e7-75dfe35ced9d>
2014-08-18 07:00:16 +0000

    #671 after the plant launch

--- phx-cnc-ui-mainform/src/CXAdditionalOperDirectionWindow.cpp
+++ phx-cnc-ui-mainform/src/CXAdditionalOperDirectionWindow.cpp
@@ -22,72 +22,72 @@
 #define V_AXIS_MAX_VALUE 5600
 
 int CXAdditionalOperDirectionWindow::GetAxisId(int _axisNumber){
-	return axisParam[_axisNumber].first;
+  return axisParam[_axisNumber].first;
 };
 
 int CXAdditionalOperDirectionWindow::GetAxisPosInMap(int _axisId){
-	switch (_axisId){
-		case A_AXIS_ID:
-		return axisParam.values().indexOf(QPair<int, QString>(A_AXIS_ID,"A"));
-		case C_AXIS_ID:
-		return axisParam.values().indexOf(QPair<int, QString>(C_AXIS_ID,"C"));
-		case V_AXIS_ID:
-		return axisParam.values().indexOf(QPair<int, QString>(V_AXIS_ID,"V"));
-	}
-	return -1;
+  switch (_axisId){
+  case A_AXIS_ID:
+    return axisParam.values().indexOf(QPair<int, QString>(A_AXIS_ID,"A"));
+  case C_AXIS_ID:
+    return axisParam.values().indexOf(QPair<int, QString>(C_AXIS_ID,"C"));
+  case V_AXIS_ID:
+    return axisParam.values().indexOf(QPair<int, QString>(V_AXIS_ID,"V"));
+  }
+  return -1;
 };
 
 QGroupBox* CXAdditionalOperDirectionWindow::CreateAxisGroup(QString _title, int _id, int _min, int _max){
-	QGroupBox* groupBox = new QGroupBox(this);
-	groupBox->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Fixed);
+  QGroupBox* groupBox = new QGroupBox(this);
+  groupBox->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Fixed);
 
-	QVBoxLayout* groupBoxLayout = new QVBoxLayout(groupBox);
-	groupBoxLayout->setMargin(7);
-	groupBoxLayout->setSpacing(6);
+  QVBoxLayout* groupBoxLayout = new QVBoxLayout(groupBox);
+  groupBoxLayout->setMargin(7);
+  groupBoxLayout->setSpacing(6);
 
-	QHBoxLayout* editLayout = new QHBoxLayout;
-	editLayout->setMargin(0);
-	editLayout->setSpacing(6);
+  QHBoxLayout* editLayout = new QHBoxLayout;
+  editLayout->setMargin(0);
+  editLayout->setSpacing(6);
 
-	//
-	QLabel* lAxisName = new QLabel(_title, groupBox);
-	editLayout->addWidget(lAxisName, 1);
+  //
+  QLabel* lAxisName = new QLabel(_title, groupBox);
+  editLayout->addWidget(lAxisName, 1);
 
-	curAxisPos.append(new QSpinBox(groupBox));
-	curAxisPos[nAxis]->setReadOnly(true);
-	curAxisPos[nAxis]->setButtonSymbols(QAbstractSpinBox::NoButtons);
-	curAxisPos[nAxis]->setMinimum(_min);
-	curAxisPos[nAxis]->setMaximum(_max);
-	editLayout->addWidget(curAxisPos[nAxis],4);
+  curAxisPos.append(new QSpinBox(groupBox));
+  curAxisPos[nAxis]->setReadOnly(true);
+  curAxisPos[nAxis]->setButtonSymbols(QAbstractSpinBox::NoButtons);
+  curAxisPos[nAxis]->setMinimum(-10000);
+  curAxisPos[nAxis]->setMaximum(10000);
+  editLayout->addWidget(curAxisPos[nAxis],4);
 
-	setAxisPos.append(new QSpinBox(groupBox));
-	setAxisPos[nAxis]->setButtonSymbols(QAbstractSpinBox::NoButtons);
-	setAxisPos[nAxis]->setMinimum(_min);
-	setAxisPos[nAxis]->setMaximum(_max);
-	editLayout->addWidget(setAxisPos[nAxis],4);
+  setAxisPos.append(new QSpinBox(groupBox));
+  setAxisPos[nAxis]->setButtonSymbols(QAbstractSpinBox::NoButtons);
+  setAxisPos[nAxis]->setMinimum(-10000);
+  setAxisPos[nAxis]->setMaximum(10000);
+  editLayout->addWidget(setAxisPos[nAxis],4);
 
-	QLabel* lVal = new QLabel(trUtf8("0.1") + QChar(176), groupBox);
-	editLayout->addWidget(lVal,2);
+  QLabel* lVal = new QLabel(trUtf8("0.1") + QChar(176), groupBox);
+  editLayout->addWidget(lVal,2);
 
-	groupBoxLayout->addLayout(editLayout);
+  groupBoxLayout->addLayout(editLayout);
 
-	QHBoxLayout* buttonLayout = new QHBoxLayout;
-	buttonLayout->setMargin(0);
-	buttonLayout->setSpacing(6);
+  QHBoxLayout* buttonLayout = new QHBoxLayout;
+  buttonLayout->setMargin(0);
+  buttonLayout->setSpacing(6);
 
-	//
-	butRelMove.append(new CXTouchButton(trUtf8("Относительно"), groupBox));
-	buttonLayout->addWidget(butRelMove[nAxis]);
-	butAbsMove.append(new CXTouchButton(trUtf8("Абсолютно"), groupBox));
-	buttonLayout->addWidget(butAbsMove[nAxis]);
+  //
+  butRelMove.append(new CXTouchButton(trUtf8("Относительно"), groupBox));
+  buttonLayout->addWidget(butRelMove[nAxis]);
+  butAbsMove.append(new CXTouchButton(trUtf8("Абсолютно"), groupBox));
+  buttonLayout->addWidget(butAbsMove[nAxis]);
 
-	groupBoxLayout->addLayout(buttonLayout);
+  groupBoxLayout->addLayout(buttonLayout);
 
-	//записываем значение
-	axisParam.insert(nAxis,QPair<int, QString>(_id,_title));
-	//увеличиваем количество осей
-	nAxis++;
-	return groupBox;
+  //записываем значение
+  axisParam.insert(nAxis,QPair<int, QString>(_id,_title));
+  //увеличиваем количество осей
+  nAxis++;
+  return groupBox;
 };
 
 CXAdditionalOperDirectionWindow::CXAdditionalOperDirectionWindow() {
@@ -102,25 +102,25 @@ CXAdditionalOperDirectionWindow::CXAdditionalOperDirectionWindow() {
   centralLayout->addStretch();
 
   QHBoxLayout* buttonLayout = new QHBoxLayout;
-	buttonLayout->setSpacing(10);
+  buttonLayout->setSpacing(10);
 
-	backButton = new CXTouchButton(trUtf8("<<"), NULL);
-	//backButton->setSizePolicy(QSizePolicy::Preferred,QSizePolicy::Minimum);
-	buttonLayout->addWidget(backButton,1);
+  backButton = new CXTouchButton(trUtf8("<<"), NULL);
+  //backButton->setSizePolicy(QSizePolicy::Preferred,QSizePolicy::Minimum);
+  buttonLayout->addWidget(backButton,1);
 
-	keyboardButton = new CXTouchButton(trUtf8("Клавиатура"), NULL);
-	//keyboardButton->setSizePolicy(QSizePolicy::Preferred,QSizePolicy::Minimum);
-	buttonLayout->addWidget(keyboardButton,2);
+  keyboardButton = new CXTouchButton(trUtf8("Клавиатура"), NULL);
+  //keyboardButton->setSizePolicy(QSizePolicy::Preferred,QSizePolicy::Minimum);
+  buttonLayout->addWidget(keyboardButton,2);
 
-	centralLayout->addLayout(buttonLayout);
+  centralLayout->addLayout(buttonLayout);
 
   connect(backButton, SIGNAL(clicked()), this, SLOT(onBackWindow()));
   connect(keyboardButton, SIGNAL(clicked()), this, SLOT(onStartKeyboard()));
   connect(mUdpManager, SIGNAL(commandReceived(const QString&, const QString&, const QString&)),
-          this, SLOT(onCommandReceive(const QString&, const QString&, const QString&)));
+      this, SLOT(onCommandReceive(const QString&, const QString&, const QString&)));
   for(int i = 0; i < nAxis; i++){
-  	connect(butRelMove[i], SIGNAL(clicked()), this, SLOT(onComporativeMove()));
-  	connect(butAbsMove[i], SIGNAL(clicked()), this, SLOT(onAbsoluteMove()));
+    connect(butRelMove[i], SIGNAL(clicked()), this, SLOT(onComporativeMove()));
+    connect(butAbsMove[i], SIGNAL(clicked()), this, SLOT(onAbsoluteMove()));
   }
 
   registerManager();
@@ -129,49 +129,49 @@ CXAdditionalOperDirectionWindow::CXAdditionalOperDirectionWindow() {
 }
 
 CXAdditionalOperDirectionWindow::~CXAdditionalOperDirectionWindow() {
-	// TODO Auto-generated destructor stub
+  // TODO Auto-generated destructor stub
 }
 
 void CXAdditionalOperDirectionWindow::onStartKeyboard(){
-	keyProcess = new CXProcess();
-	keyProcess->start("./startKeyboard.sh");
-	connect(keyProcess, SIGNAL(finished(int, QProcess::ExitStatus))
-				, this, SLOT(onKeyboardFinished(int, QProcess::ExitStatus)));
+  keyProcess = new CXProcess();
+  keyProcess->start("./startKeyboard.sh");
+  connect(keyProcess, SIGNAL(finished(int, QProcess::ExitStatus))
+      , this, SLOT(onKeyboardFinished(int, QProcess::ExitStatus)));
 };
 
 void CXAdditionalOperDirectionWindow::onBackWindow(){
-	hide();
-	emit backWindow();
+  hide();
+  emit backWindow();
 };
 
 void CXAdditionalOperDirectionWindow::onComporativeMove(){
-	for (int i = 0; i < nAxis; i++){
-		if (sender() == butRelMove[i])
-			{
-				QString res("%1=%2");
-				res = res.arg(GetAxisId(i)).arg(setAxisPos[i]->value() * 100);
-				mUdpManager->sendCommand(Commands::MSG_SECTION_OPERATOR, Commands::MSG_CMD_HAND_COMPARATIVE_MOVING, res);
-			}
-	};
+  for (int i = 0; i < nAxis; i++){
+    if (sender() == butRelMove[i])
+    {
+      QString res("%1=%2");
+      res = res.arg(GetAxisId(i)).arg(setAxisPos[i]->value() * 100);
+      mUdpManager->sendCommand(Commands::MSG_SECTION_OPERATOR, Commands::MSG_CMD_HAND_COMPARATIVE_MOVING, res);
+    }
+  };
 };
 
 void CXAdditionalOperDirectionWindow::onAbsoluteMove(){
-	for (int i = 0; i < nAxis; i++){
-			if (sender() == butAbsMove[i])
-				{
-					QString res("%1=%2");
-				  res = res.arg(GetAxisId(i)).arg(setAxisPos[i]->value() * 100);
-				  mUdpManager->sendCommand(Commands::MSG_SECTION_OPERATOR, Commands::MSG_CMD_HAND_ABSOLUTE_MOVING, res);
-				}
-		};
+  for (int i = 0; i < nAxis; i++){
+    if (sender() == butAbsMove[i])
+    {
+      QString res("%1=%2");
+      res = res.arg(GetAxisId(i)).arg(setAxisPos[i]->value() * 100);
+      mUdpManager->sendCommand(Commands::MSG_SECTION_OPERATOR, Commands::MSG_CMD_HAND_ABSOLUTE_MOVING, res);
+    }
+  };
 };
 
 void CXAdditionalOperDirectionWindow::onKeyboardFinished(int aExitCode, QProcess::ExitStatus aExitStatus){
-	if (keyProcess != NULL)
-	{
-		keyProcess->deleteLater();
-		keyProcess = NULL;
-	}
+  if (keyProcess != NULL)
+  {
+    keyProcess->deleteLater();
+    keyProcess = NULL;
+  }
 }
 
 void
